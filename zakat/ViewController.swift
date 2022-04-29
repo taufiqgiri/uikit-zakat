0d766a582aecac93e0c613f71010866294b24504
taufiqgiri <taufiq.ichwanusofa@ist.id>
2022-04-29 08:11:39 +0700

    Initial Commit

---
+++ zakat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zakat
+//
+//  Created by Taufiq Ichwanusofa on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
